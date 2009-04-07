4917398e47ecd39975ba654f55a31ae5c4879c7e
Fabio Veronez <fabio.veronez@gmail.com>
2009-04-07 09:31:06 -0300

    added example code for ref equality and call to stack reduceWith method

--- src/br/fcv/tests/scala/Fake.scala
+++ src/br/fcv/tests/scala/Fake.scala
@@ -5,13 +5,33 @@ import java.io
 object Fake extends Application {
   
   private def filesHere = new io.File(".").listFiles
-  
+    
   private def get = {
     for (file <- filesHere; if file.isDirectory) 
       yield file
   }
   
-  get foreach { println(_) }
+  get foreach { a => println(a.getCanonicalPath()) } 
+    // {  println(_) }
     
-
+  import java.lang.Integer;
+  
+  val one1 = new Integer(1);
+  val one2 = new Integer(1);
+  
+  println("==: " + (one1 == one2 )) //-- prints true
+  println("equals: " + (one1 equals one2) ) //-- prints true
+  println("eq: " + (one1 eq one2) ) //-- false 
+  
+  
+  import collection.mutable.ArrayStack;
+  var stack = new ArrayStack[Int];
+  stack ++= List(1,2,3,4,5)
+  println;
+  println( stack );
+  stack reduceWith {(a, b) => a+b} 
+  
+  println( stack );
+  
 }
+
