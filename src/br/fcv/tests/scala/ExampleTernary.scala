27859ed5188a5aaa0682b4f3dbf0670e33a509d4
Fabio Veronez <fabio.veronez@gmail.com>
2009-04-07 09:32:22 -0300

    contextualizing import of TernaryOps

--- src/br/fcv/tests/scala/ExampleTernary.scala
+++ src/br/fcv/tests/scala/ExampleTernary.scala
@@ -3,7 +3,7 @@ package br.fcv.tests.scala
 object ExampleTernary extends Application {
   
   object TernaryOp {
-    implicit def fakeTernary[A](a:(A,A)) = new{
+    implicit def fakeTernary[A](a:(A,A)) = new {
       def ?:(p:Boolean)=if(p) a._1 else a._2
     }  
     //Before adding the Tuplicator class the compiler got very confused
@@ -12,27 +12,28 @@ object ExampleTernary extends Application {
     // -- This will, in fact, add ~: operator at all  classes ...
     // -- that is not something i would like
 
-    //class Tuplicator[A](a:A){
-    //  def ~:(b:A) = (b,a)
-    //}
-    //implicit def tuplicate[A](a:A) = new Tuplicator(a)
+    class Tuplicator[A](a:A){
+      def ~:(b:A) = (b,a)
+    }
+    implicit def tuplicate[A](a:A) = new Tuplicator(a)
   }
 
-  import TernaryOp._
-  
-  var a = {
-    "teste"
+  {
+	// -- import is visible only inside this context bounds  
+    import TernaryOp._
+	  
+	var a = {
+	  "teste"
+	}
+	  
+	var b = {
+	  "tunico"
+	}
+	  
+	println( "before ternary call" )
+	println( (1>3) ?: a ~: b )
   }
   
-  var b = {
-    "tunico"
-  }
-  
-  println( "before ternary call" )
-  println( (1>3) ?: (a, b) )
-  
-
-
-  
+  //-- it is not possible to call '?' operation over Boolean here!!
   
 }
