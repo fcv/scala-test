2a90992acc55d0048b7c6c81c56bbccec9c77773
Fabio Veronez <fabio.veronez@gmail.com>
2009-04-16 14:43:24 -0300

    simple example of xml handling

---
+++ src/br/fcv/tests/scala/xml/SimpleExample.scala
@@ -0,0 +1,25 @@
+package br.fcv.tests.scala.xml
+
+import _root_.scala.xml.NodeSeq.Empty
+
+object FirstTry extends Application {
+  
+  val n = 
+<my-node>
+  <a prop="my-prop"></a>
+  <b>
+    <b-a>content of ba</b-a>
+  </b> 
+</my-node>
+  
+  
+  val n1 = n \ "b-a"
+  
+  println(n1 == Empty) //-- prints "true"
+  println(n1) //-- prints nothing
+  println(n \ "a" \ "@prop") //-- prints "my-prop" 
+  println(n \\ "b-a") //-- prints "<b-a>content of ba</b-a>"
+  println(n \ "c" \ "b-a") //-- prints nothing but gives no exceptions ... sounds good
+  println(n \ "b" \ "b-a") //-- prints "<b-a>content of ba</b-a>"
+ 
+}
