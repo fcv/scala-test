8466b88c63e16c3392afa005052272c3576c6840
Fabio Veronez <fabio.veronez@gmail.com>
2009-04-17 10:27:08 -0300

    added Email example of Extractors

---
+++ src/br/fcv/tests/scala/extractors/Emails.scala
@@ -0,0 +1,46 @@
+package br.fcv.tests.scala.extractors
+
+/**
+ * Example of Extractors usage taken from Programming in Scala book
+ */
+object Email extends Application {
+
+  object Email {
+    def apply(user:String, domain: String) = user + "@" + domain
+    
+    def unapply(email: String): Option[(String, String)] = {
+      val parts = email split "@"
+      if (parts.length == 2) Some(parts(0), parts(1)) else None
+    }
+  }
+  
+  object Domain {
+    def apply(subdomains: String*):String = subdomains.reverse.mkString(".") 
+    def unapplySeq(domain: String): Option[Seq[String]] = {
+      val subdomains = domain split "\\."
+      Some( subdomains reverse )
+    } 
+  }
+  
+  def printEmailFields(email: String) = {
+    email match {
+	  case Email(user, domain) => println("user: " + user + ", domain: " + domain)
+	  case _ => println("'" + email + "' is a valid email")
+	}
+  }
+  
+  def printUsersFromBrServers(email: String) = {
+    email match {
+      case Email(user, Domain("br", _*)) => println (email)
+      case _ => //-- just ignore
+    }
+  }
+  
+  
+  printEmailFields("genesio@server.com.br")
+  printEmailFields("someordinarystring")
+  
+  println
+  
+  List("genesio@server.com.br", "invalidemail", "user@notbrserver.com", "baltazar@anotherserver.com.br") foreach printUsersFromBrServers _  
+}
