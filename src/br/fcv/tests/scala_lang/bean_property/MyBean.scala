bc5422a57be3bdb0ecd803d9a8579dc862dbb34a
Fabio Veronez <fabio.veronez@gmail.com>
2009-04-23 09:24:37 -0300

    changing import from 'import _root_.scala..' to only 'import scala..'
    since packaging has been changed to 'br.fcv.tests.scala_lang' there is no more "conflict" of import name for 'scala'

--- src/br/fcv/tests/scala_lang/bean_property/MyBean.scala
+++ src/br/fcv/tests/scala_lang/bean_property/MyBean.scala
@@ -1,6 +1,6 @@
 package br.fcv.tests.scala_lang.bean_property
 
-import _root_.scala.reflect.{BeanProperty => Prop}
+import scala.reflect.{BeanProperty => Prop}
 import java.util.Date
 
 class MyBean(@Prop var name: String, @Prop var birthdate: Date) {
